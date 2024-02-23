c1caf200fdb2391d920562cf679ba054694f8706
felix <felixyeah@outlook.com>
2024-02-23 08:56:55 -0800

    Create onunload.js

---
+++ onunload.js
@@ -0,0 +1,6 @@
+window.addEventListener("beforeunload", function (e) {
+    var confirmationMessage = 'It looks like you have been editing something. '
+                            + 'If you leave before saving, your changes will be lost.';
+    (e || window.event).returnValue = confirmationMessage;
+    return confirmationMessage;
+});
